780d992812be7ed0991b9ade2494298e300e7b76
AzathothDefiler <59626506+AzathothDefiler@users.noreply.github.com>
2023-07-09 12:22:27 -0400

    scrolltex

---
+++ Assets/Scripts/ScrollMatTex.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMatTex : MonoBehaviour
+{
+	public GameObject quadGameObject;
+	private Renderer quadRenderer;
+
+	float scrollTexSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        quadRenderer = quadGameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2(Time.time*scrollSpeed,0);
+		quadRenderer.material.mainTextureOffset = textureOffset;
+    }
+}
